e08181a3aa9e82ff650d10502bab7fc1647ffb72
ITS - Mauricio Munoz <mauricio.munoz@its.com.co>
2023-01-30 12:41:19 -0500

    schema.sql modificado

--- packages/database/schema.sql
+++ packages/database/schema.sql
@@ -9,8 +9,8 @@ CREATE TABLE Coordenada (
 CREATE TYPE medio_pago_tipo AS ENUM ('Debito', 'Credito');
 
 CREATE TABLE Medio_Pago (
-  -- medio_pago_id SERIAL PRIMARY KEY,
-  numero_cuenta VARCHAR(255) PRIMARY KEY,
+  medio_pago_id SERIAL PRIMARY KEY,
+  numero_cuenta VARCHAR(255) UNIQUE,
   tipo medio_pago_tipo NOT NULL
 );
 
@@ -27,7 +27,7 @@ CREATE TABLE Usuario (
 CREATE TABLE Cliente (
     cliente_id SERIAL PRIMARY KEY,
     recibo VARCHAR(255) NOT NULL,
-    numero_cuenta VARCHAR(255) REFERENCES Medio_Pago(medio_pago_id) UNIQUE NOT NULL,
+    numero_cuenta VARCHAR(255) REFERENCES Medio_Pago(numero_cuenta) UNIQUE NOT NULL,
     user_id INTEGER REFERENCES Usuario(user_id) UNIQUE NOT NULL
 );
 
@@ -117,7 +117,7 @@ $$ LANGUAGE plpgsql;
 
 
 --Añadir un cliente
-CREATE OR REPLACE FUNCTION crear_cliente(
+CREATE OR REPLACE PROCEDURE crear_cliente(
   nombre VARCHAR(255),
   apellido VARCHAR(255),
   email VARCHAR(255),
@@ -127,18 +127,18 @@ CREATE OR REPLACE FUNCTION crear_cliente(
   direccion VARCHAR(255),
   recibo VARCHAR(255),
   celular VARCHAR(255),
-  tipo_cuenta VARCHAR(255)
-) RETURNS INTEGER AS $$  
+  num_cuenta VARCHAR(255),
+  tipo_cuenta medio_pago_tipo
+) AS $$  
 BEGIN
   -- Crear una nueva fila en la tabla Coordenada
-  INSERT INTO Coordenada (latitud, longitud, direccion) VALUES (latitud, longitud, direccion) RETURNING coor_id;
+  INSERT INTO Coordenada (latitud, longitud, direccion) VALUES (latitud, longitud, direccion);
   -- Crear una nueva fila en la tabla Usuario con la relación a la coordenada recién creada
-  INSERT INTO Usuario (nombre, apellido, email, contrasena, celular, coor_id) VALUES (nombre, apellido, email, contrasena, celular, coor_id) RETURNING user_id;
+  INSERT INTO Usuario (nombre, apellido, email, contrasena, celular, coor_id) VALUES (nombre, apellido, email, contrasena, celular, (SELECT coor_id FROM Coordenada ORDER BY coor_id DESC LIMIT 1));
   -- Crear una nueva fila en la tabla Medio_Pago
-  INSERT INTO Medio_Pago (tipo) VALUES (tipo_cuenta) RETURNING numero_cuenta;
+  INSERT INTO Medio_Pago (numero_cuenta,tipo) VALUES (num_cuenta,tipo_cuenta::medio_pago_tipo);
   -- Crear una nueva fila en la tabla Cliente con la relación al usuario recién creado y al medio de pago recién creado
-  INSERT INTO Cliente (recibo, numero_cuenta, user_id) VALUES (recibo, numero_cuenta, user_id);
-  RETURN user_id;
+  INSERT INTO Cliente (recibo, numero_cuenta, user_id) VALUES (recibo, (SELECT num_cuenta FROM Medio_Pago ORDER BY medio_pago_id DESC LIMIT 1), (SELECT user_id FROM Usuario ORDER BY user_id DESC LIMIT 1));
 END;
 $$ LANGUAGE plpgsql;
 
@@ -146,7 +146,7 @@ $$ LANGUAGE plpgsql;
 --Verificar que un email este en uso, devuelve true si esta en uso
 CREATE OR REPLACE FUNCTION verificar_email(email_ VARCHAR(255)) RETURNS TABLE(id INTEGER) AS $$
 BEGIN
-SELECT id FROM Usuario WHERE email = email_ INTO id;
+SELECT user_id FROM Usuario U WHERE U.email = email_ INTO id;
   IF FOUND THEN
       RETURN QUERY SELECT id;
   ELSE
@@ -170,14 +170,14 @@ END;
 $$ LANGUAGE plpgsql;
 
 --Verificar loguin de cliente
-CREATE OR REPLACE FUNCTION verificar_login_cliente(user_id_ INTEGER) RETURNS INTEGER AS $$
+CREATE OR REPLACE FUNCTION verificar_login_cliente(user_id_ INTEGER) RETURNS TABLE (id INTEGER) AS $$
 BEGIN
   -- Consultar en la tabla Cliente si el usuario encontrado en la tabla Usuario tiene un trabajador asociado
-  SELECT cliente_id FROM Cliente WHERE user_id = user_id_;
+  SELECT cliente_id FROM Cliente WHERE user_id = user_id_ INTO id;
   IF FOUND THEN
-    RETURN cliente_id;
+    RETURN QUERY SELECT id;
   ELSE
-    RETURN -1;
+    RETURN QUERY SELECT NULL::INTEGER as id;
   END IF;
 END;
 $$ LANGUAGE plpgsql;
@@ -202,37 +202,13 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION verificar_celular(celular_ VARCHAR(255)) RETURNS TABLE (id INTEGER) AS $$
 BEGIN
   -- Consultar en la tabla Usuario si existe un usuario con el número de celular dado
-  SELECT id FROM Usuario WHERE celular = celular_ INTO id;
+  SELECT user_id FROM Usuario WHERE celular = celular_ INTO id;
   -- Si existe al menos una fila con el número de celular dado, retornar true, en otro caso, false
   IF FOUND THEN
       RETURN QUERY SELECT id;
   ELSE
       RETURN QUERY SELECT NULL::INTEGER as id;
   END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-
-
--- Insertar un labor de un trabajador
-CREATE OR REPLACE FUNCTION crear_relacion_ejerce(trabajador_id INTEGER, labor VARCHAR(255), tipo_trabajo VARCHAR(255), precio FLOAT, descripcion VARCHAR(255))
-RETURNS BOOLEAN AS $$
-DECLARE
-labor_id INTEGER;
-BEGIN
--- Verificar si el labor ya existe
-SELECT labor_id INTO labor_id FROM Labor WHERE labor = labor;
-IF NOT FOUND THEN
--- Crear nueva fila en la tabla Labor
-INSERT INTO Labor (labor) VALUES (labor) RETURNING labor_id INTO labor_id;
-END IF;
--- Crear nueva fila en la tabla Ejerce
-INSERT INTO Ejerce (trabajador_id, labor_id, tipo_trabajo, precio, descripcion) 
-VALUES (trabajador_id, labor_id, tipo_trabajo, precio, descripcion);
-RETURN true;
-EXCEPTION
-WHEN unique_violation THEN
-RETURN false;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -253,7 +229,6 @@ LANGUAGE plpgsql;
 CREATE TRIGGER actualizar_disponibilidad
 AFTER INSERT ON Contrato
 FOR EACH ROW EXECUTE FUNCTION actualizar_disponibilidad();
-
 
 
 -- Trigger para actualizar el estado de disponibilidad de un trabajador al finalizar un contrato
@@ -282,7 +257,7 @@ CREATE OR REPLACE FUNCTION update_promedio_calificacion()
 RETURNS TRIGGER AS $$
 BEGIN
   IF (NEW.calificacion IS NOT NULL) THEN
-    UPDATE Trabajador SET calificacion = (SELECT AVG(calificacion) FROM Contrato WHERE trabajador_id = (SELECT trabajador_id FROM Ejerce WHERE ejerce_id = NEW.ejerce_id)) WHERE trabajador_id = (SELECT trabajador_id FROM Ejerce WHERE ejerce_id = NEW.ejerce_id);
+    UPDATE Trabajador SET calificacion = (SELECT AVG(calificacion) FROM Contrato WHERE trabajador_id = (SELECT trabajador_id FROM Ejerce WHERE ejerce_id = NEW.ejerce_id));
   END IF;
   RETURN NEW;
 END;
@@ -293,70 +268,10 @@ AFTER UPDATE ON Contrato
 FOR EACH ROW
 EXECUTE FUNCTION update_promedio_calificacion();
 
-
--- Vista para mostrar información de los contratos de un cliente
-CREATE VIEW contratos_cliente AS
-SELECT contrato_id, ejerce.labor_id, labor, fecha_i, fecha_f, calificacion
-FROM Contrato 
-JOIN Ejerce ON Contrato.ejerce_id = Ejerce.ejerce_id
-JOIN Labor ON Ejerce.labor_id = Labor.labor_id;
-
 --Un usuario solicita un servicio
--- CREATE EXTENSION postgis;
--- CREATE EXTENSION cube;
--- CREATE EXTENSION earthdistance;
-
-CREATE OR REPLACE FUNCTION buscar_trabajadores(
-  labor_id_in INT,
-  latitud_in FLOAT,
-  longitud_in FLOAT,
-  criterio VARCHAR(255)
-) RETURNS TABLE(
-  trabajador_id INTEGER,
-  nombre VARCHAR(255),
-  apellido VARCHAR(255),
-  calificacion INT,
-  precio FLOAT,
-  tipo_trabajo VARCHAR(255),
-  descripcion VARCHAR(255),
-  distancia FLOAT
-) AS $$
-BEGIN
-  RETURN QUERY
-  SELECT
-  t.trabajador_id,
-  u.nombre,
-  u.apellido,
-  t.calificacion,
-  e.precio,
-  e.tipo_trabajo,
-  e.descripcion,
-  earth_distance(ll_to_earth(latitud_in, longitud_in), ll_to_earth(c.latitud, c.longitud)) AS distancia
-  FROM Ejerce e
-  JOIN Trabajador t ON e.trabajador_id = t.trabajador_id
-  JOIN Usuario u ON u.user_id = t.user_id
-  JOIN Coordenada c ON c.coor_id = u.coor_id
-  WHERE (e.labor_id = labor_id_in OR (labor_id_in = 0 OR labor_id_in IS NULL)) AND t.disponible = true
-  ORDER BY
-  CASE criterio
-  WHEN 'calificacion' THEN t.calificacion
-  WHEN 'precio' THEN e.precio
-  ELSE distancia
-  END;
-END;
-$$ LANGUAGE plpgsql;
-
---Crear un contrato
-CREATE OR REPLACE FUNCTION crear_contrato(ejerce_id INTEGER, cliente_id INTEGER, descripcion VARCHAR(255))
-RETURNS INTEGER AS $$
-	DECLARE
-	transaccion_id INTEGER;
-BEGIN
-	INSERT INTO Transaccion (fecha, monto) VALUES (NULL, NULL) RETURNING transaccion_id;
-	INSERT INTO Contrato (ejerce_id, cliente_id, descripcion, calificacion, fecha_i, fecha_f, transaccion_id) VALUES (ejerce_id, cliente_id, descripcion, NULL, NOW(), NULL, transaccion_id) RETURNING contrato_id;
-	RETURN contrato_id;
-END;
-$$ LANGUAGE plpgsql;
+CREATE EXTENSION postgis;
+CREATE EXTENSION cube;
+CREATE EXTENSION earthdistance;
 
 --Trigger para notificacion
 CREATE OR REPLACE FUNCTION enviar_notificacion()
@@ -391,55 +306,7 @@ END;
 $BODY$
 LANGUAGE plpgsql;
 
-
 CREATE TRIGGER notificar_usuario
 AFTER INSERT ON Contrato
 FOR EACH ROW
 EXECUTE FUNCTION enviar_notificacion();
-
--- Funcion para saber que trabajo esta haciendo un trabajador
-CREATE OR REPLACE FUNCTION trabajo_actual(trabajador_id INTEGER)
-RETURNS TABLE (contrato_id INTEGER, ejerce_id INTEGER, cliente_id INTEGER, calificacion INTEGER, fecha_i DATE, fecha_f DATE) AS $$
-BEGIN
-  RETURN QUERY SELECT contrato_id, ejerce_id, cliente_id, calificacion, fecha_i, fecha_f FROM Contrato 
-  WHERE trabajador_id = trabajador_id AND fecha_f IS NULL;
-END;
-$$ LANGUAGE plpgsql;
-
---Funcion para calificar y pagar
-CREATE OR REPLACE FUNCTION calificar_y_pagar(contrato_id INTEGER, calificacion INTEGER, monto FLOAT)
-RETURNS VOID AS $$
-BEGIN
-  UPDATE Contrato SET calificacion = calificacion WHERE contrato_id = contrato_id;
-  UPDATE Transaccion SET fecha = NOW(), monto = monto WHERE transaccion_id = (SELECT transaccion_id FROM Contrato WHERE contrato_id = contrato_id);
-END;
-$$ LANGUAGE plpgsql;
-
--- Todos los pagos hechos por clientes
-CREATE OR REPLACE FUNCTION obtener_pagos_cliente(cliente_id INTEGER)
-RETURNS TABLE(transaccion_id INTEGER, fecha DATE, monto FLOAT)
-AS
-$BODY$
-BEGIN
-  RETURN QUERY
-  SELECT transaccion_id, fecha, monto
-  FROM Transaccion
-  WHERE transaccion_id IN (SELECT transaccion_id FROM Contrato WHERE cliente_id = cliente_id);
-END;
-$BODY$
-LANGUAGE plpgsql;
-
--- Todos los pagos hechos a trabajadores
-CREATE OR REPLACE FUNCTION pagos_trabajadores(IN trabajador_id INTEGER)
-RETURNS TABLE(fecha DATE, monto FLOAT, numero_cuenta INTEGER) AS
-$BODY$
-BEGIN
-  RETURN QUERY
-    SELECT trans.fecha, trans.monto, med.numero_cuenta
-    FROM Transaccion trans
-    JOIN Contrato con ON trans.transaccion_id = con.transaccion_id
-    JOIN Medio_Pago med ON med.numero_cuenta = con.numero_cuenta
-    WHERE con.trabajador_id = trabajador_id;
-END;
-$BODY$
-LANGUAGE plpgsql;
